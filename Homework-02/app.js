a6d10b8bff8f865a8b6b68f9363a90154818e552
SergeyShkolniy <s.shkolniy@gmail.com>
2022-02-18 12:28:50 +0200

    homework-02

--- Homework-02/app.js
+++ Homework-02/app.js
@@ -4,8 +4,8 @@ const {engine} = require('express-handlebars');
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('view engine', '.hbs');
@@ -15,29 +15,39 @@ app.set('views', path.join(__dirname, 'static'));
 const users = [];
 
 app.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login');
 });
 
-app.get('/users', (req, res) => {
-    res.render('users', {users})
-    console.log(users)
-});
+app.get('/users', (req, res) =>{
+
+    if (Object.keys(req.query).length){
+        let usersQuery = [...users];
+
+        if (req.query.age){
+            usersQuery = usersQuery.filter(user => user.age === req.query.age);
+        }
+        if (req.query.city){
+            usersQuery = usersQuery.filter(user => user.city === req.query.city);
+        }
+        res.render('users', {users: usersQuery});
+        return;
+    }
+    res.render('users', {users});
+} )
 
 app.get('/users/:userId', (req, res) => {
-
     const user = users.find(user => user.id === +req.params.userId);
 
     if (!user){
         console.log('Такого пользователя нет!!!');
         res.redirect('/notFound');
     }else{
-        res.render('user', {user})
-        console.log(user)
+        res.render('user', {user});
     }
 })
 
 app.get('/error', (req, res) => {
-    res.render('error')
+    res.render('error');
 });
 
 app.post('/login', (req, res) => {
@@ -57,5 +67,5 @@ app.use((req, res) => {
 })
 
 app.listen(5200, () => {
-    console.log('server rabotaet');
+    console.log('Сервер работает');
 })
