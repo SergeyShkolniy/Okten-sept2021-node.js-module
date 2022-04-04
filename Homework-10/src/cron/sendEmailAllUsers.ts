048e1a46157aabd47bd1e6b9b2376d4683603729
SergeyShkolniy <s.shkolniy@gmail.com>
2022-04-04 20:31:56 +0300

    cron sendEmailAllUsers

--- Homework-10/src/cron/sendEmailAllUsers.ts
+++ Homework-10/src/cron/sendEmailAllUsers.ts
@@ -7,15 +7,20 @@ import { EmailActionEnum } from '../email';
 export const sendEmailAllUsers = async () => {
     cron.schedule('*/10 * * * * *', async () => {
         console.log('sendEmailAllUsers');
+
         const allUsers = await userRepository.sendEmailAllUsers();
-        Promise.all(allUsers).then((results) => results
-            .forEach((result) => emailService
-                .sendMail(
-                    result.email,
-                    EmailActionEnum.ALL_USERS,
-                    {
-                        firstName: result.firstName,
-                    },
-                )));
+
+        const allEmailUsers: any [] = [];
+        allUsers.map((user) => allEmailUsers.push(user));
+
+        const results = await Promise.all(allEmailUsers);
+        results.forEach((result) => emailService
+            .sendMail(
+                result.email,
+                EmailActionEnum.ALL_USERS,
+                {
+                    firstName: result.firstName,
+                },
+            ));
     });
 };
